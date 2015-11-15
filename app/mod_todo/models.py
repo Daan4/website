05695cc3e4bef45d9a5321a38eabf6d5dc2f5140
Daan Lubbers <daan_lubbers@hotmail.com>
2015-11-15 02:24:24 +0100

    Created database models Todo, TodoCategory, TodoStatus and TodoPriority

--- app/mod_todo/models.py
+++ app/mod_todo/models.py
@@ -1,1 +1,35 @@
+from app import db
+from app.models import BaseModel
 
+
+class Todo(BaseModel):
+    todo = db.Column(db.Text)
+    status_id = db.Column(db.Integer, db.ForeignKey('todostatus.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('todocategory.id'))
+    closed_on = db.Column(db.DateTime)
+    do_before = db.Column(db.DateTime)
+    priority = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return '<Todo item: {}>'.format(self.id)
+
+
+class TodoCategory(BaseModel):
+    category = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Todo category: {}'.format(self.category)
+
+
+class TodoStatus(BaseModel):
+    status = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Todo status: {}'.format(self.status)
+
+
+class TodoPriority(BaseModel):
+    priority = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Todo priority: {}'.format(self.priority)
