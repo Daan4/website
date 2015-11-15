be168a9d59ecbe68230fa660ac35c2a5165a5df7
Daan Lubbers <daan_lubbers@hotmail.com>
2015-11-15 17:33:10 +0100

    Added support for running more than 1 test class at once

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -29,4 +29,11 @@ class BaseTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    from tests.mod_auth_tests import TestAuth
+    test_classes_to_run = [TestAuth]
+    # Run all test cases in test_classes_to_run
+    loader = unittest.TestLoader()
+    suites_list = [loader.loadTestsFromTestCase(x) for x in test_classes_to_run]
+    big_suite = unittest.TestSuite(suites_list)
+    runner = unittest.TextTestRunner()
+    results = runner.run(big_suite)
