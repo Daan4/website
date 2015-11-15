107832a7292b2b356e1cf057088c3eb3bcb75d6b
Daan Lubbers <daan_lubbers@hotmail.com>
2015-11-15 15:43:30 +0100

    Removed auto update functionality

--- app/mod_streams/stream_api.py
+++ app/mod_streams/stream_api.py
@@ -5,14 +5,13 @@ from urllib.error import URLError
 
 from app import app, db
 from app.mod_streams.models import Stream
+# todo: stream api shouldnt import website_config directly
+# todo: get api stream url from config
 from config import website_config as c
 
 TWITCH_API_CLIENT_ID = c.TWITCH_API_CLIENT_ID
 TWITCH_API_CLIENT_SECRET = c.TWITCH_API_CLIENT_SECRET
 TWITCH_API_STREAM_URL = 'https://api.twitch.tv/kraken/streams/?channel='
-
-automatic_update_interval = 0
-automatic_updates = False
 
 
 def get_twitch_stream_object(channels):
@@ -48,15 +47,3 @@ def update_stream_info(auto_update=True):
                     record.viewers = stream['viewers']
                     break
     db.session.commit()
-    global automatic_updates, automatic_update_interval
-    if automatic_updates and auto_update:
-        threading.Timer(automatic_update_interval, update_stream_info).start()
-
-
-def enable_updates(interval):
-    global automatic_updates, automatic_update_interval
-    if not automatic_updates:
-        app.logger.info("Now updating stream info every {} seconds.".format(interval))
-        automatic_update_interval = interval
-        automatic_updates = True
-        update_stream_info()
