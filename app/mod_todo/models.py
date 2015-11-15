743df7ef0dce72ae3ddfb4ab51f1ec1b968b09f6
Daan Lubbers <daan_lubbers@hotmail.com>
2015-11-15 02:27:00 +0100

    Updated database models Todo, TodoCategory, TodoStatus and TodoPriority

--- app/mod_todo/models.py
+++ app/mod_todo/models.py
@@ -3,33 +3,34 @@ from app.models import BaseModel
 
 
 class Todo(BaseModel):
-    todo = db.Column(db.Text)
+    todo = db.Column(db.Text, index=True)
     status_id = db.Column(db.Integer, db.ForeignKey('todostatus.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('todocategory.id'))
+    priority_id = db.Column(db.Integer, db.ForeignKey('todopriority.id'))
     closed_on = db.Column(db.DateTime)
     do_before = db.Column(db.DateTime)
-    priority = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return '<Todo item: {}>'.format(self.id)
 
 
 class TodoCategory(BaseModel):
-    category = db.Column(db.String(64), unique=True)
+    category = db.Column(db.String(64), unique=True, index=True)
 
     def __repr__(self):
         return '<Todo category: {}'.format(self.category)
 
 
 class TodoStatus(BaseModel):
-    status = db.Column(db.String(64), unique=True)
+    status = db.Column(db.String(64), unique=True, index=True)
 
     def __repr__(self):
         return '<Todo status: {}'.format(self.status)
 
 
 class TodoPriority(BaseModel):
-    priority = db.Column(db.String(64), unique=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    priority = db.Column(db.Integer, unique=True)
 
     def __repr__(self):
         return '<Todo priority: {}'.format(self.priority)
