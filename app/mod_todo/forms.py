1f91c1f18e6378fb5d616745a8b1edb92584b8e3
Daan Lubbers <daan_lubbers@hotmail.com>
2015-11-16 21:46:31 +0100

    Created adminpanel for todo module to create categories and priorities

--- app/mod_todo/forms.py
+++ app/mod_todo/forms.py
@@ -1,1 +1,23 @@
+from wtforms import *
+from flask_wtf import Form
+from wtforms.validators import *
+from app.validators import *
+from app.fields import *
 
+
+class CreateTodoItemForm(Form):
+    todo = TextAreaField('Todo', validators=[DataRequired()])
+    category = NonValidatingSelectField('Category')
+    priority = NonValidatingSelectField('Priority')
+    do_before = DateTimeField('Do before')
+    create = SubmitField('Create')
+
+
+class TodoConfigForm(Form):
+    category = StringField('Category', validators=[RequiredWhenFieldsHaveData('create_category', 'delete_category')])
+    create_category = SubmitField('Create category')
+    delete_category = SubmitField('Delete category')
+    priority_name = StringField('Priority name', validators=[RequiredWhenFieldsHaveData('create_priority', 'delete_priority')])
+    priority_value = IntegerField('Priority value', validators=[RequiredWhenFieldsHaveData('create_priority'), Optional()])
+    create_priority = SubmitField('Create priority')
+    delete_priority = SubmitField('Delete priority')
--- app/mod_todo/views.py
+++ app/mod_todo/views.py
@@ -1,9 +1,99 @@
-from flask import render_template, Blueprint
+from flask import *
+from .forms import *
+from .models import *
+from sqlalchemy.exc import *
+from sqlalchemy.orm.exc import *
+from app.mod_adminpanel.views import register_adminpanel
 
 mod_todo = Blueprint('todo', __name__, url_prefix='/todo', template_folder='templates',
                      static_folder='static')
 
 
-@mod_todo.route('/')
+@mod_todo.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('todo.html')
+    form = CreateTodoItemForm()
+    form.category.choices = [(c.id, c.category) for c in TodoCategory.query.all()]
+    form.priority.choices = [(p.id, p.name) for p in TodoPriority.query.all()]
+    if form.validate_on_submit():
+        if form.create.data:
+            todo = form.todo.data
+            category = form.category.data
+            priority = form.priority.data
+            do_before = form.do_before.data
+            add_todo_item(todo, category, priority, do_before)
+            flash('New todo item created')
+    return render_template('todo.html', form=form)
+
+
+def add_todo_item(todo, category_id, priority_id, do_before):
+    pass
+
+
+def close_todo_item(todo_id):
+    pass
+
+
+def remove_todo_item(todo_id):
+    pass
+
+
+@register_adminpanel(mod_todo.name)
+def do_adminpanel_logic():
+    form = TodoConfigForm()
+    if form.validate_on_submit():
+        category = form.category.data
+        priority_name = form.priority_name.data
+        priority_value = form.priority_value.data
+        if form.create_category.data:
+            create_todo_category(category)
+        elif form.delete_category.data:
+            delete_todo_category(category)
+        elif form.create_priority.data:
+            create_todo_priority(priority_name, priority_value)
+        elif form.delete_priority.data:
+            delete_todo_priority(priority_name)
+    return render_template('todo_config.html', form=form, title='Admin Panel - ToDo')
+
+
+def create_todo_category(category):
+    new_category = TodoCategory(category=category)
+    try:
+        db.session.add(new_category)
+        db.session.commit()
+        flash('Category {} added'.format(category))
+    except (IntegrityError, InvalidRequestError):
+        db.session.rollback()
+        flash('Category {} already exists'.format(category))
+
+
+def delete_todo_category(category):
+    existing_category = TodoCategory.query.filter_by(category=category).first()
+    try:
+        db.session.delete(existing_category)
+        db.session.commit()
+        flash('Category {} removed'.format(category))
+    except UnmappedInstanceError:
+        db.session.rollback()
+        flash('Category {} doesn\'t exist'.format(category))
+
+
+def create_todo_priority(name, value):
+    new_priority = TodoPriority(name=name, priority=value)
+    try:
+        db.session.add(new_priority)
+        db.session.commit()
+        flash('Priority {} added'.format(name))
+    except (IntegrityError, InvalidRequestError):
+        db.session.rollback()
+        flash('Priority {} already exists'.format(name))
+
+
+def delete_todo_priority(name):
+    existing_priority = TodoPriority.query.filter_by(name=name).first()
+    try:
+        db.session.delete(existing_priority)
+        db.session.commit()
+        flash('Priority {} removed'.format(name))
+    except UnmappedInstanceError:
+        db.session.rollback()
+        flash('Priority {} doesn\'t exist'.format(name))
