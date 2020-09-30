f60b940205a5e1011ce1c9c5672cb262c4649c0b
Daan4 <1859384+Daan4@users.noreply.github.com>
2020-09-30 21:29:05 +0200

    Validate username and password on signup

--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, EqualTo
+from .models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,7 +9,13 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
+
 class SignupForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
-    password2 = PasswordField('password2', validators=[DataRequired()])
+    password2 = PasswordField('password2', validators=[DataRequired(), EqualTo('password')])
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already in use. Please use a different username.')
