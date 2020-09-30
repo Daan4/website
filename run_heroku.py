2f75bea12fd13b8eda0e224e5eddefb237244516
Daan4 <1859384+Daan4@users.noreply.github.com>
2020-09-30 21:57:43 +0200

    Update Heroku Config

--- run_heroku.py
+++ run_heroku.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app('config.heroku_config')
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
