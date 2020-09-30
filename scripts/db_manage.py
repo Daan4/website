2bebda30bbdb2322a4f229f9547012ece910df55
Daan4 <1859384+Daan4@users.noreply.github.com>
2020-09-30 22:18:16 +0200

    fix db scripts for heroku

--- scripts/db_manage.py
+++ scripts/db_manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
+from werkzeug.utils import ImportStringError
 
 # script parameters
 # db init : creates migrations folder
@@ -8,7 +9,11 @@ from app import db, create_app
 # db upgrade [<revision>] : upgrade database to revision
 # db downgrade [<revision>] : downgrade database to revision
 
-app = create_app('config.config')
+try:
+    app = create_app('config.config')
+except ImportStringError:
+    app = create_app('config.heroku_config')
+
 
 # Flask-Migrate
 migrate = Migrate(app, db, compare_type=True)
