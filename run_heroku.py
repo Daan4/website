e18106dcaac33cc382732befc2dd274d06adaa27
Daan4 <1859384+Daan4@users.noreply.github.com>
2020-09-30 21:57:43 +0200

    Update Heroku Config

--- run_heroku.py
+++ run_heroku.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app('config.heroku_config')
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=False, use_reloader=False)
