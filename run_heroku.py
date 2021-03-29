96eb466bc2db241c37ab4e1c6f2186c472b4ef39
Daan4 <1859384+Daan4@users.noreply.github.com>
2021-03-29 21:34:24 +0200

    switch to waitress for WSGI

--- run_heroku.py
+++ run_heroku.py
@@ -1,8 +1,9 @@
 from app import create_app
 import os
+import waitress
 
 app = create_app('config.heroku_config')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+    waitress.serve(app, host='0.0.0.0', port=port, url_scheme='https')
