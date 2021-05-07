93f41219d8655b20091b8b1048057fd5febaf75b
Daan4 <1859384+Daan4@users.noreply.github.com>
2021-05-07 23:55:06 +0200

    login error fix for heroku

--- config/heroku_config.py
+++ config/heroku_config.py
@@ -7,7 +7,9 @@ WTF_CSRF_ENABLED = True  # Activates cross-site request forgery prevention (CSRF
 SECRET_KEY = os.environ.get('SECRET_KEY')  # Secret key used for CSRF
 
 # Flask-SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_CYAN_URL')
+# For the URI postgres:// is deprecated and replaced by postgresql:// ,
+# however changing the config var involves removing the postgres addon so therefore this replacement instead.
+SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_CYAN_URL').replace('postgres://', 'postgresql://')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')  # SQLAlchemy-migrate data file directory
 
