3dc59924f7f8dcaf52af69490c7d79c996bafb1f
Daan4 <1859384+Daan4@users.noreply.github.com>
2021-05-07 23:54:52 +0200

    nano donation placeholder

--- app/mod_nano/views.py
+++ app/mod_nano/views.py
@@ -6,3 +6,7 @@ mod_nano = Blueprint('nano', __name__, url_prefix='/nano', template_folder='temp
 @mod_nano.route('/overview')
 def overview():
     return render_template('overview.html')
+
+@mod_nano.route('/donate')
+def donate():
+    return render_template('donate.html')
